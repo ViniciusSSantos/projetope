ab17ba823176c7d596bb0d7289bde2169f2cda7f
Matheus Shimizu <40467263+x-mizu@users.noreply.github.com>
2021-03-17 20:08:17 -0300

    Remove acesso .txt e armazena em variavel global

--- criptografia.c
+++ criptografia.c
@@ -17,208 +17,139 @@ struct client{
     struct transferencias transferencias[50];
 };
 
-// void encrypt(char *, char *, int);
-void encrypt(struct client *cliente, char *);
-void decrypt(char *);
-void login(char *, char *, int *);
-void saveUser(char *);
-void saveTransfer(int *);
-void itoa(int, char *);
-void reverse(char *);
-void getContaDisponivel(char *);
-void cadastraCliente(struct client *cliente);
+struct client bancoDados[1000];
+
+void encrypt(struct client *client);
+void decrypt(struct client *client);
+void saveClient(struct client cliente);
+struct client getClientByConta(char *conta);
+char* getContaDisponivel();
 
 void main(){
-    srand(time(NULL)); //necessário para o rand()
+    while(1) {
+        int id;
+        printf("Bem vindo ao Banco XXXXX, pressione 1 para fazer login e 2 para criar uma nova conta: ");
+        scanf("%d", &id);
+        
+        switch(id){
+            case 1:
+                
+                printf("Conta: ");
+                struct client cliente;
+                scanf("%s", cliente.conta);
+              
+                char senha[50];
+                printf("Senha: ");
+                scanf("%s", senha);
 
-    int id;
-    printf("Bem vindo ao Banco XXXXX, pressione 1 para fazer login e 2 para criar uma nova conta: ");
-    scanf("%d", &id);
-    char usuarioDecrypt[50], senhaDecrypt[50];
-    struct client cliente;
-    int conta;
+                cliente = getClientByConta(cliente.conta);
+                decrypt(&cliente);
 
-    switch(id){
+                if(strcmp(senha, cliente.senha) == 0){
+                    printf("Olá %s\n",cliente.nome);
+                
+                    // menu depois de ter logado
+                    while(1) { 
+
+                    }
+                
+                } else
+                    printf("Senha incorreta ou conta inexistente.\n");
+
+                break;
+
+            case 2:
+                
+                printf("Nome: ");
+                struct client novoCliente;
+                scanf("%s", novoCliente.nome);
+
+                printf("Senha: ");
+                scanf("%s", novoCliente.senha);
+
+                encrypt(&novoCliente);
+                strncpy(novoCliente.conta, getContaDisponivel(&novoCliente.conta), 5);
+                saveClient(novoCliente);
+
+                printf("Sua conta é: %s, salve esse número, você precisará dele para acessar sua conta.\n", novoCliente.conta);
+                struct client cliente2 = getClientByConta(novoCliente.conta); // se eu tirar essa linha, comeca a dar ruim na hora de gravar, nao sei pq
+
+                break;
+
+        }
+    }
+}
+
+void encrypt(struct client *client) {
+    for(int i = 0; i < strlen(client->nome); i++){
+        (*client).nome[i] += 2;
+    }
+
+    for(int i = 0; i < strlen(client->senha); i++){
+        (*client).senha[i] += 2;
+    }
+}
+
+void decrypt(struct client *client){
+
+    for(int i = 0; i < strlen(client->nome); i++){
+        (*client).nome[i] -= 2;
+    }
+
+    for(int i = 0; i < strlen(client->senha); i++){
+        (*client).senha[i] -= 2;
+    }
+}
 
 
-        case 1:
-
-            printf("Usuario: \n");
-            scanf("%s", usuarioDecrypt);
-
-            printf("Senha: \n");
-            scanf("%s", senhaDecrypt);
-
-            printf("Agência: ");
-            scanf("%d", &conta);
-
-            // encrypt(usuarioDecrypt, senhaDecrypt, conta);
-
-            /*
-            Aqui a variável usuarioDecrypt estará já encriptada, da mesma forma que estará salva no .txt
-            Então acredito que aqui o ideal seja verificar, linha a linha, o .txt em busca de um match para validar o login do usuário
-            */
-
+void saveClient(struct client cliente) {
+    for (int i = 0; i < 1000; i++) {
+        if (strcmp(bancoDados[i].conta, cliente.conta) == 0
+        || strcmp(bancoDados[i].conta, "") == 0) {
+            bancoDados[i] = cliente;
             break;
+        }
+    }
+}
 
-        case 2:
-            printf("Nome: ");
-            scanf("%s", cliente.nome);
+struct client getClientByConta(char *conta) {
+    struct client *cliente = malloc(sizeof(struct client));
 
-            printf("Senha: ");
-            scanf("%s", cliente.senha);
-
-            cadastraCliente(&cliente);
-
-            printf("Sua conta é: %s, salve esse número, você precisará dele para acessar sua conta.\n", cliente.conta);
-
+    for (int i = 0; i < 1000; i++) {
+        if (strcmp(bancoDados[i].conta, conta) == 0) {
+            cliente = &bancoDados[i];
             break;
-
-    }
-}
-
-// funcao para cadastrar um cliente
-void cadastraCliente(struct client *cliente) {
-    // busca numero da conta disponivel e atribuiu ao cliente
-    getContaDisponivel(cliente->conta);
-
-    // criptografa as informacoes do cliente
-    char usuarCrypt[200];
-    encrypt(cliente, usuarCrypt);
-
-    saveUser(usuarCrypt);
-}
-
-// void encrypt(char *usuario, char *senha, int conta){
-
-//     int i;
-//     char str[20];
-
-//     itoa(conta, str);
-
-//     strcat(usuario, "-"); //Aqui esotu adicionando apenas esse traco para nao ocorrer o erro de termos dois usuarios diferentes mas com a mesma criptografia
-//     strcat(usuario, senha);
-//     strcat(usuario, "-");
-//     strcat(usuario, str);
-
-//     for(i=0; i < strlen(usuario); i++){
-//         usuario[i] += 5;
-//     }
-
-// }
-
-void encrypt(struct client *client, char *str){
-
-    int i;
-    char aux[200], saldo[10];
-
-    itoa(client->saldo, saldo);
-
-
-    strcpy(aux, client->nome);
-    strcat(aux, "/");
-    strcat(aux, client->senha);
-    strcat(aux, "/");
-    strcat(aux, saldo);
-
-
-    for(i=0; i < strlen(aux); i++){
-        aux[i] += 2;
+        }
     }
 
-    strcpy(str, client->conta);
-    strcat(str, "1");
-    strcat(str, aux);
-
-}
-
-void decrypt(char *str){
-
-    int i;
-
-    for(i=5; i < strlen(str); i++){
-        str[i] -= 2;
-    }
-}
-
-
-void saveUser(char *str){
-    FILE *fp;
-
-    fp = fopen("./usuarios.txt", "a");
-    fputs(str, fp);
-    fputs("\n", fp);
-    fclose(fp);
+    return *cliente;
 }
 
 // busca a ultima conta que foi cadastrada e retorna o numero da proxima conta
-void getContaDisponivel(char * novaConta) {
-    // abre o arquivo de usuarios no modo leitura
-    FILE *fp = fopen("./usuarios.txt", "r");
+char* getContaDisponivel() {
 
     // inicializa variaveis
-    char* registro = NULL;
-    size_t size = 0;
-    char ultimaConta[5] = "00000";
+    char *ultimaConta = malloc(5);
+    strncpy(ultimaConta, "00000", 5);
 
     // percorre todas as linhas até chegar na ultima
-    // while (getline(&registro, &size, fp) != EOF) {
-    //     strncpy(ultimaConta, registro, 5);
-    // }
+    for (int i = 0; i < 1000; i++) {
+        if (strlen(bancoDados[i].conta) > 0 )
+            strncpy(ultimaConta, bancoDados[i].conta, 5);
+        else 
+            break;
+    }
 
     // converte a ultima conta para inteiro e soma 1
     int contaNumerico = atoi(ultimaConta) + 1;
 
+    char conta[5];
     // copia o numero da conta para uma nova string
-    sprintf(novaConta, "%d", contaNumerico);
+    sprintf(conta, "%d", contaNumerico);
+    
+    for (int i = 4; i >= 0; i--)
+        if (conta[4 - i])
+            ultimaConta[i] = conta[4 - i];
 
-    // desloca a string para deixar o numero da conta a direita
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 4; j++) {
-                if (novaConta[j] != 0 && novaConta[j + 1] == 0) {
-                    char aux = novaConta[j];
-                    novaConta[j] = novaConta[j + 1];
-                    novaConta [j + 1] = aux;
-            }
-        }
-    }
-
-    // completa com zeros a esquerda
-    for (int i = 0; i < 5; i++)
-        if (novaConta[i] == 0)
-            novaConta[i] = '0';
-
-    // fecha arquivo de texto
-    fclose(fp);
+    return ultimaConta;
 }
-
-
-//Essas duas próximas funções servem pra transformar um int em uma string. Em javascrpit seria como um parseInt()
- void reverse(char *s)
- {
-     int i, j;
-     char c;
-
-     for (i = 0, j = strlen(s)-1; i<j; i++, j--) {
-         c = s[i];
-         s[i] = s[j];
-         s[j] = c;
-     }
-}
-
- void itoa(int n, char *s)
- {
-     int i, sign;
-
-     if ((sign = n) < 0)  /* record sign */
-         n = -n;          /* make n positive */
-     i = 0;
-     do {       /* generate digits in reverse order */
-         s[i++] = n % 10 + '0';   /* get next digit */
-     } while ((n /= 10) > 0);     /* delete it */
-     if (sign < 0)
-         s[i++] = '-';
-     s[i] = '\0';
-     reverse(s);
-}
-
