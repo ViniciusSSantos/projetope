e46d8596159ace29dfed90dd158ce3d1613581f9
gustavoueti <gustavo.ueti@gmail.com>
2021-05-01 03:23:35 +0000

    Parte do Gustavo - Jogo 21

--- Arcade.c
+++ Arcade.c
@@ -3,6 +3,8 @@
 #include <time.h>
 #include <conio.h>
 
+void vinteeum();
+
 void main()
 {
     printf("==========   Bem vindo ao Arcade do Juca   ==========");
@@ -11,12 +13,15 @@ void main()
     {
         int id;
         printf("\n==========          Menu inicial          ==========");
-        printf("\n1 - Space Wars\n0 - Sair do programa\n");
-        scanf_s("%d", &id);
+        printf("\n1 - Space Wars\n2 - 21\n\n0 - Sair do programa\n");
+        scanf("%d", &id);
         switch (id)
         {
         case 1:
             spaceWarsGame();
+            break;
+        case 2:
+            vinteeum();
             break;
         case 0:
             arcade = 0;
@@ -92,7 +97,7 @@ int spaceWarsGame()
             else // atualiza a matriz de jogo
                 updatePlayer(playerInput, gameBoard);
         }
-       
+
     }
 }
 
@@ -180,3 +185,192 @@ void updatePlayer(int playerInput, int** gameBoard)
         break;
     }
 }
+//------------------------------------------------------------------------------------------------------------------------------------------------
+//Parte do Gustavo
+
+struct Node {
+    int value;
+    struct Node *next;
+};
+
+struct Node* createNode(int value){
+     struct Node *node = (struct Node*) malloc(sizeof(struct Node));
+     node->value = value;
+
+     return node;
+};
+
+void vinteeum(){
+    srand(time(NULL));
+    int numeroJogador = rand()%9 + 1;
+    int numeroMesa = rand()%9 + 1;
+    int opcao;
+    struct Node *jogador[1];
+    struct Node *mesa[1];
+
+    printf("==========          Jogo do 21!          ==========\n\n");
+
+    jogador[0] = createNode(numeroJogador);
+    mesa[0] = createNode(numeroMesa);
+
+    struct Node *nodeJogador = jogador[0];
+    struct Node *nodeMesa = mesa[0];
+
+    printf("Mão da mesa: %d\n\n",numeroMesa);
+    printf("Sua mão: %d\n\n",numeroJogador);
+
+    while(opcao != 2){
+        printf("Selecione a opção que deseja:\n(1)- Comprar mais uma carta\n(2)- Sair\n");
+        printf("\n");
+        scanf("%d", &opcao);
+
+        if(opcao == 1){
+            int totalMesa1=0;
+            struct Node *nodePercorreMesa1 = mesa[0];
+
+            while(nodePercorreMesa1){
+                totalMesa1 += nodePercorreMesa1->value;
+                nodePercorreMesa1 = nodePercorreMesa1->next;
+            }
+
+            if(totalMesa1 <18){
+                int totalMesa2=0;
+                struct Node *nodePercorreMesa2 = mesa[0];
+                int numeroMesa = rand()%9 + 1;
+
+
+                while(nodePercorreMesa2){
+                    if(nodePercorreMesa2->next == NULL){
+                        nodePercorreMesa2 -> next = createNode(numeroMesa);
+                        break;
+                    }
+                    nodePercorreMesa2 = nodePercorreMesa2->next;
+                }
+                printf("===================================================\n\n");
+                printf("Mão da mesa: ");
+                struct Node *nodePercorreMesa3 = mesa[0];
+                while(nodePercorreMesa3){
+                    printf("%d ", nodePercorreMesa3->value);
+                    totalMesa2 += nodePercorreMesa3->value;
+                    nodePercorreMesa3 = nodePercorreMesa3->next;
+                }
+                printf("\nTotal da Mesa: %d\n", totalMesa2);
+                printf("\n");
+                printf("\n");
+                if(totalMesa2 >= 21){
+                    break;
+                }
+
+            }else{
+                printf("===================================================\n\n");
+                printf("Mão da mesa: ");
+                int totalMesa2=0;
+                struct Node *nodePercorreMesa3 = mesa[0];
+                while(nodePercorreMesa3){
+                    printf("%d ", nodePercorreMesa3->value);
+                    totalMesa2 += nodePercorreMesa3->value;
+                    nodePercorreMesa3 = nodePercorreMesa3->next;
+                }
+                printf("\nTotal da Mesa: %d\n", totalMesa2);
+                printf("\n");
+                printf("\n");
+            }
+            int numeroJogador = rand()%9 + 1;
+            struct Node *nodePercorreJogador = jogador[0];
+
+            while(nodePercorreJogador){
+                if(nodePercorreJogador->next == NULL){
+                    nodePercorreJogador -> next = createNode(numeroJogador);
+                    break;
+                }
+                nodePercorreJogador = nodePercorreJogador->next;
+            }
+            printf("Sua mão: ");
+            int total=0;
+            struct Node *nodePercorreJogador2 = jogador[0];
+            while(nodePercorreJogador2){
+                printf("%d ", nodePercorreJogador2->value);
+                total += nodePercorreJogador2->value;
+                nodePercorreJogador2 = nodePercorreJogador2->next;
+            }
+            printf("\nTotal sua mão: %d\n", total);
+            printf("\n");
+            printf("\n");
+            printf("===================================================\n\n");
+            if(total>=21){
+                break;
+            }
+        }
+
+    }
+
+    int mesaValidacao=0;
+    struct Node *nodePercorreMesaValidacao = mesa[0];
+
+    while(nodePercorreMesaValidacao){
+        mesaValidacao += nodePercorreMesaValidacao->value;
+        nodePercorreMesaValidacao = nodePercorreMesaValidacao->next;
+    }
+
+    while(mesaValidacao <=16){
+        int totalMesa4=0;
+        struct Node *nodePercorreMesa4 = mesa[0];
+        int numeroMesa4 = rand()%9 + 1;
+
+
+        while(nodePercorreMesa4){
+            if(nodePercorreMesa4->next == NULL){
+                nodePercorreMesa4 -> next = createNode(numeroMesa4);
+                break;
+            }
+            nodePercorreMesa4 = nodePercorreMesa4->next;
+        }
+        printf("Mão da mesa: ");
+        struct Node *nodePercorreMesa5 = mesa[0];
+        while(nodePercorreMesa5){
+            printf("%d ", nodePercorreMesa5->value);
+            totalMesa4 += nodePercorreMesa5->value;
+            nodePercorreMesa5 = nodePercorreMesa5->next;
+        }
+        printf("\nTotal da Mesa: %d\n", totalMesa4);
+        printf("\n");
+        printf("\n");
+        mesaValidacao = totalMesa4;
+    }
+
+    int mesaFinal=0;
+    struct Node *nodePercorreMesaFinal = mesa[0];
+
+    while(nodePercorreMesaFinal){
+        mesaFinal += nodePercorreMesaFinal->value;
+        nodePercorreMesaFinal = nodePercorreMesaFinal->next;
+    }
+
+    int jogadorFinal=0;
+    struct Node *nodePercorreJogadorFinal = jogador[0];
+
+    while(nodePercorreJogadorFinal){
+        jogadorFinal += nodePercorreJogadorFinal->value;
+        nodePercorreJogadorFinal = nodePercorreJogadorFinal->next;
+    }
+    printf("===================================================\n\n");
+
+    if(mesaFinal > 21){
+        printf("Você Ganhou!!\n\n");
+    } else if(jogadorFinal>21){
+        printf("Você perdeu!!\n\n");
+    }else{
+        if(mesaFinal > jogadorFinal){
+        printf("Você Perdeu!!\n\n");
+        } else if(mesaFinal < jogadorFinal){
+            printf("Você Ganhou!!\n\n");
+        }else{
+            printf("Empatou!!\n\n");
+        }
+    }
+    printf("===================================================\n\n");
+
+}
+
+
+//------------------------------------------------------------------------------------------------------------
